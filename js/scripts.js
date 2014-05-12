90ef178a9938f69d4db422bcd6dd965fae68f367
Elizabeth Morrison <emorrison2011@eagles.ewu.edu>
2014-05-12 13:48:04 -0700

    big video trial 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,10 @@
 // My Scripts
 $(document).foundation();
+
+
+//Big Video
+$(function() {
+    var BV = new $.BigVideo();
+    BV.init();
+    BV.show('http://github.com/eamwords/doh_404_pages/oceans.mp4');
+});
